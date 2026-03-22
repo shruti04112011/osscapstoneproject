e3561c55b9789168c8fc02972cf9dc319216b3b5
shruti04112011 <shruti.24boe10005@vitbhopal.ac.in>
2026-03-23 00:40:47 +0530

    Create script4.sh

---
+++ script4.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Script 4: Log File Analyzer
+# Author: Shruti Banik
+
+LOGFILE=$1
+KEYWORD=${2:-"error"}
+COUNT=0
+
+if [ ! -f "$LOGFILE" ]; then
+    echo "Error: File not found."
+    exit 1
+fi
+
+while IFS= read -r LINE; do
+    if echo "$LINE" | grep -iq "$KEYWORD"; then
+        COUNT=$((COUNT + 1))
+    fi
+done < "$LOGFILE"
+
+echo "Keyword '$KEYWORD' found $COUNT times in $LOGFILE"
+
+echo ""
+echo "Last 5 matching lines:"
+grep -i "$KEYWORD" "$LOGFILE" | tail -5
